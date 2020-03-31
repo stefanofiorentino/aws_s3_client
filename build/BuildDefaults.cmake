f3f320452e95f5768f16340a3d1c924150a3aa08
Stefano Fiorentino <stefano.fiore84@gmail.com>
2020-03-31 15:47:31 +0200

    fix bad importing behaviors of aws-checksums
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- build/BuildDefaults.cmake
+++ build/BuildDefaults.cmake
@@ -1,5 +1,4 @@
 # Append custom CMake modules.
-list(APPEND CMAKE_MODULE_PATH /usr/local/lib)
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/cmake)
 
-include(FetchContentAwsSdkCpp)
+include(ExternalProject_Add_AwsSdkCpp)
